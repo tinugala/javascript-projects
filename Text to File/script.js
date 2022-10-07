b5476883409bfc9efb35ce293666e90ad39ae0b1
tinugala <tinugala786@gmail.com>
2022-10-07 11:29:51 +0530

    adding new js  projects

---
+++ Text to File/script.js
@@ -0,0 +1,18 @@
+const textArea = document.querySelector("textarea"),
+fileNameInput = document.querySelector(".file-name input"),
+selectMenu = document.querySelector(".save-as select"),
+saveBtn = document.querySelector(".save-btn");
+
+selectMenu.addEventListener("change",()=>{
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save as ${selectedOption.split(" ")[0]} File`;
+});
+
+saveBtn.addEventListener("click",()=> {
+    const blob =new Blob([textArea.value], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = fileNameInput.value;
+    link.href = fileUrl;
+    link.click();
+});
